e7e7a6fa1f5b2e59358ede4bc5583788b87a0bc5
Bill <darkbluemn@gmail.com>
2015-10-22 16:05:52 -0400

    commit

--- app.js
+++ app.js
@@ -5,19 +5,97 @@
                 {
                     id: 1,
                     title: 'BMW 3 Series',
-                    current: {},
+                    rules: [
+                        {
+                            name: 'Class A',
+                            rate: 100
+                        },
+                        {
+                            name: 'Class B',
+                            rate: 80
+                        },
+                        {
+                            name: 'Class C',
+                            rate: 60
+                        },
+                        {
+                            name: 'Class D',
+                            rate: 40
+                        }
+                    ],
+                    base: 20,
+                    isRun: false,
+                    current: {
+                        AClicks: 0,
+                        BClicks: 0,
+                        CClicks: 0,
+                        DClicks: 0,
+                        Balance: 5000000
+                    },
                     history: []
                 },
                 {
                     id: 2,
                     title: 'BMW 5 Series',
-                    current: {},
+                    rules: [
+                        {
+                            name: 'Class A',
+                            rate: 100
+                        },
+                        {
+                            name: 'Class B',
+                            rate: 80
+                        },
+                        {
+                            name: 'Class C',
+                            rate: 60
+                        },
+                        {
+                            name: 'Class D',
+                            rate: 40
+                        }
+                    ],
+                    base: 40,
+                    isRun: false,
+                    current: {
+                        AClicks: 0,
+                        BClicks: 0,
+                        CClicks: 0,
+                        DClicks: 0,
+                        Balance: 8000000
+                    },
                     history: []
                 },
                 {
                     id: 3,
                     title: 'BMW 7 Series',
-                    current: {},
+                    rules: [
+                        {
+                            name: 'Class A',
+                            rate: 100
+                        },
+                        {
+                            name: 'Class B',
+                            rate: 80
+                        },
+                        {
+                            name: 'Class C',
+                            rate: 60
+                        },
+                        {
+                            name: 'Class D',
+                            rate: 40
+                        }
+                    ],
+                    base: 70,
+                    isRun: false,
+                    current: {
+                        AClicks: 0,
+                        BClicks: 0,
+                        CClicks: 0,
+                        DClicks: 0,
+                        Balance: 10000000
+                    },
                     history: []
                 }
             ];
@@ -47,15 +125,60 @@
                 return campaignsService.getCurrentID() === id;
             };
         })
+        .directive('ruleChart', function() {
+            return {
+                template: '<canvas width="550" height="300"></canvas>',
+                restrict: 'E',
+                link: function (scope, element) {
+                    var data = {
+                        labels : [],
+                        datasets : [
+                            {
+                                fillColor : "rgba(151,187,205,0.5)",
+                                strokeColor : "rgba(151,187,205,1)",
+                                pointColor : "rgba(151,187,205,1)",
+                                pointStrokeColor : "#fff",
+                                data : []
+                            }
+                        ]
+                    }
+                    scope.campaign.rules.forEach(function(element) {
+                        data.labels.push(element.name);
+                        data.datasets[0].data.push(element.rate);
+                    });
+                    var ctx = element[0].firstChild.getContext("2d");
+                    var myNewChart = new Chart(ctx).Line(data, {
+
+                        //Boolean - If we want to override with a hard coded scale
+                        scaleOverride : true,
+
+                        //** Required if scaleOverride is true **
+                        //Number - The number of steps in a hard coded scale
+                        scaleSteps : 10,
+                        //Number - The value jump in the hard coded scale
+                        scaleStepWidth : 10,
+                        //Number - The scale starting value
+                        scaleStartValue : 0,
+                        //Boolean - Whether to show labels on the scale
+                        scaleShowLabels : true,
+
+                        //Interpolated JS string - can access value
+                        scaleLabel : "<%=value%>%",
+                    });
+                }
+            };
+        })
         .directive('slider', function() {
             return {
-                template: '<div class="col-md-4 form-inline"></div><div class="col-md-8" style="padding-top: 7px"><div style="background: #0081BB"></div></div>',
+                template: '<div style="background: #0081BB"></div>',
                 restrict: 'E',
-                link: function (scope, element, attrs) {
-                    element[0].firstChild.innerHTML = '<div class="input-group"><div class="input-group-addon">Class ' + attrs.fraudRate + '</div><input type="text" class="form-control" value="' + attrs.default + '" style="width: 50px;" /><div class="input-group-addon">%</div></div>';
+                require: 'ngModel',
+                link: function (scope, element, attrs, ctrl) {
+                    var rangeSlider = element[0].firstChild;
 
-                    noUiSlider.create(element[0].childNodes[1].childNodes[0], {
-                        start: 40,
+                    noUiSlider.create(rangeSlider, {
+                        start: scope.rule.rate,
+                        step: 10,
                         connect: 'lower',
                         range: {
                             'min': [ 0 ],
@@ -63,6 +186,10 @@
                         }
                     });
 
+                    rangeSlider.noUiSlider.on('update', function( values, handle ) {
+                        ctrl.$setViewValue(Math.ceil(values[handle]));
+                        ctrl.$render();
+                    });
                 }
             }
         });
